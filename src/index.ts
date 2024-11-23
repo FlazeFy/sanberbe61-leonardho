c6fb870ca748b6334b1f06acb1ffe1c5bb9fd3b0
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-11-23 19:13:37 +0700

    [DEPLOY] Swagger css fixes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import db from "./utils/database";
 import routes from "./routes/api";
 import bodyParser from "body-parser";
 import { swaggerUi, specs } from './docs/swagger'
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 const PORT = 3000;
 
 async function init() {
@@ -13,7 +14,13 @@ async function init() {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+    // app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs, {
+        customCss:
+            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+        customCssUrl: CSS_URL,
+    }
+    ))
     app.use("/api", routes);
 
     app.listen(PORT, () => {
